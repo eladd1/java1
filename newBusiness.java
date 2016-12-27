c50be5a6ce1041fc292a575f48490cbe487bd022
eladd1 <eladeross@gmail.com>
2016-12-27 15:56:16 +0200

    התחלנו מסך הוספת עסק חדש

---
+++ newBusiness.java
@@ -0,0 +1,13 @@
+package com.example.elad.android5777;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newBusiness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_business);
+    }
+}
